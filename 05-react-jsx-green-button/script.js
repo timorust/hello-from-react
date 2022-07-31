2098122d3b9b6e1fb265184c6eb9d84d6e47559f
Timor Rust <timorust1@gmail.com>
2022-07-31 16:40:50 +0300

    props

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,16 +1,20 @@
-const App = () => {
+const App = ({ initialButtonText }) => {
 
-	const [buttonText, setButtonText] = React.useState('Click me')
+	const [buttonText, setButtonText] = React.useState(initialButtonText)
+	const [classesList, setClassesList] = React.useState('')
+
 	const onButtonClick = () => {
-		buttonText = 'Hello from React'
+		setButtonText('Hello from React')
+		setClassesList('green-btn')
 	}
+
 	return (
 		<div className="app">
-			<button onClick={onButtonClick}>{buttonText}</button>
+			<button className={classesList} onClick={onButtonClick}>{buttonText}</button>
 		</div>
 	)
 }
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
-root.render(<App />)
+root.render(<App initialButtonText="Click me" />)
